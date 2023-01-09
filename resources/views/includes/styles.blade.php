335174e63228e4364f483c185eb8d03467a45c13
mdaushi <firdaus.ex19@gmail.com>
2023-01-09 23:05:26 +0700

    add tabler

---
+++ resources/views/includes/styles.blade.php
@@ -0,0 +1,4 @@
+<link href="{{ asset("./tabler/dist/css/tabler.min.css") }}" rel="stylesheet"/>
+<link href="{{ asset("./tabler/dist/css/tabler-flags.min.css") }}" rel="stylesheet"/>
+<link href="{{ asset("./tabler/dist/css/tabler-payments.min.css") }}" rel="stylesheet"/>
+<link href="{{ asset("./tabler/dist/css/tabler-vendors.min.css") }}" rel="stylesheet"/>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("layouts.base");
+    // return view('welcome');
+});
+
+Route::middleware(['auth'])->prefix("dashboard")->group(function () {
+    Route::get("/", [DashboardController::class, "index"]);
 });
